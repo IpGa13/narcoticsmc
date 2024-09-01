988b05f17880c969d2fb5c0f0d6924b400b88c5b
Fredeun <ipga13@fredeun.net>
2024-09-01 17:58:58 +0200

    Initial Project Commit

---
+++ src/main/java/net/fredeun/narcotics/NarcoticsMCDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fredeun.narcotics;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NarcoticsMCDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
